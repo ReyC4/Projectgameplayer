a67ae1a0848214123fdc140707acddef3d43d3cd
ReyC4 <125941312+ReyC4@users.noreply.github.com>
2024-03-04 12:31:48 +0700

    Add files via upload

---
+++ Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator m_Animator;
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        print("ini start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("ini update");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("ini spasi diklik!");
+            m_Animator.SetTrigger("goattack");
+        }
+        
+    }
+}
